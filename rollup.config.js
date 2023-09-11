a595424f1442b37ce7c32b08f9bba1cc617e1464
chenmijiang <jack.chenyuana@gmail.com>
2023-09-12 07:53:07 +0800

    wip: add SSR judgment, set the initial value globally

--- rollup.config.js
+++ rollup.config.js
@@ -3,36 +3,51 @@ import commonjs from '@rollup/plugin-commonjs';
 import esbuild from 'rollup-plugin-esbuild';
 import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 
 export const file = (type) => `dist/index.${type}.js`;
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      name: 'rc-lf',
-      file: file('umd'),
-      format: 'umd',
-      globals: {
-        react: 'React',
+const input = 'src/index.ts';
+
+export default [
+  {
+    input,
+    output: [
+      {
+        name: 'rc-lf',
+        file: file('umd'),
+        format: 'umd',
+        globals: {
+          react: 'React',
+        },
       },
+      {
+        file: file('esm'),
+        format: 'esm',
+      },
+    ],
+    external: ['react', 'react-dom'],
+    plugins: [
+      babel({
+        babelHelpers: 'bundled',
+      }),
+      typescript(),
+      resolve({
+        preferBuiltins: true,
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }),
+      commonjs(),
+      esbuild(),
+      terser(),
+    ],
+  },
+  {
+    input,
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
     },
-    {
-      file: file('esm'),
-      format: 'esm',
-    },
-  ],
-  external: ['react', 'react-dom'],
-  plugins: [
-    babel({
-      babelHelpers: 'bundled',
-    }),
-    typescript(),
-    resolve({
-      preferBuiltins: true,
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }),
-    commonjs(),
-    esbuild(),
-  ],
-};
+    plugins: [dts()],
+  },
+];
